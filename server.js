44f69a58416326701a32c8197e23e0a260cab69f
timok181 <timo.koenig67@gmail.com>
2023-02-16 19:16:14 +0100

    Created server

---
+++ server.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', true)
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to database'))
+
+app.use(express.json())
+
+const libraryRouters = require('./routes/library')
+
+app.listen(3000, () => console.log('Server started'))
