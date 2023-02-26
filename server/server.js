93b8e2c823b5a8496c73a3439c58389bc2ac01c6
timok181 <timo.koenig67@gmail.com>
2023-02-26 18:51:10 +0100

    Added versioning routes

--- server/server.js
+++ server/server.js
@@ -11,18 +11,18 @@ const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to database'))
 
-
 app.use(express.json())
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     next();
 });
 
-
 const booksRouter = require('./routes/v1/books')
+app.use('/v1/books', booksRouter)
 app.use('/books', booksRouter)
 
 const authorsRouter = require('./routes/v1/authors.js')
+app.use('/v1/authors', authorsRouter)
 app.use('/authors', authorsRouter)
 
 app.listen(3000, () => console.log('Server started'))
