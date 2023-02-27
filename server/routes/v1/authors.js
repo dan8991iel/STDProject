d66182c22fae2627657839fe457c1a447898fe80
timok181 <timo.koenig67@gmail.com>
2023-02-27 23:35:09 +0100

    Finalized API

--- server/routes/v1/authors.js
+++ server/routes/v1/authors.js
@@ -3,100 +3,111 @@ const router = express.Router()
 const Author = require ('../../models/author')
 
 // Get list of all allowed methods (OPTIONS)
-router.options('/', async (req, res) => {
-    res.set('Allow', 'GET,HEAD').status(200).json("GET,HEAD")
+router.options('/', setOptions, async (request, response) => {
+    //response.set('Allow', 'GET,HEAD').status(200).json("GET,HEAD")
+    response.set('Allow', response.options).json(response.options)
+})
+router.options('/:id', setOptions, async (request, response) => {
+    //response.set('Allow', 'GET,HEAD').status(200).json("GET,HEAD")
+    response.set('Allow', response.options).json(response.options)
 })
 
 // Retrieve all authors (GET)
-router.get('/', async (req, res) =>{
-    //res.set('Allow', 'GET, HEAD').status(405).json({message: 'Retrieving all authors is not allowed'})
+router.get('/', async (request, response) =>{
+    //response.set('Allow', 'GET, HEAD').status(405).json({message: 'Retrieving all authors is not allowed'})
     try{
         const author = await Author.find()
-        res.status(200).json(author)
+        response.status(200).json(author)
     }catch(error){
-        res.status(500).json({message: error.message})
+        response.status(500).json({message: error.message})
     }
 })
 
 // Retrieve a specific author (GET)
-router.get('/:id', getAuthor, async (req, res) =>{
-    res.json(res.author)
+router.get('/:id', getAuthor, async (request, response) =>{
+    response.json(response.author)
 })
 
 // Create a new author (POST)
-router.post('/', async (req, res) =>{
-    //res.set('Allow', 'GET, HEAD').status(405).json({message: 'New authors cannot be created'})
+router.post('/', async (request, response) =>{
+    //response.set('Allow', 'GET, HEAD').status(405).json({message: 'New authors cannot be created'})
     const author = new Author({
         _id: 999, // only temporary, is overwritten pre-save
-        "name.firstName": req.body.name.firstName,
-        "name.surname": req.body.name.surname,
-        publications: req.body.publications,
+        "name.firstName": request.body.name.firstName,
+        "name.surname": request.body.name.surname,
+        publications: request.body.publications,
         link: "null"
     })
 
     try{
         const newAuthor = await author.save()
-        res.status(201).json(newAuthor)
+        response.status(201).json(newAuthor)
     }catch(error){
-        res.status(400).json({message: error.message})
+        response.status(400).json({message: error.message})
     }
 })
 
 // Update certain details of a specific author (PATCH)
-router.patch('/:id', /*getAuthor,*/ async (req, res) =>{
-    //res.set('Allow', 'GET, HEAD').status(405).json({message: 'Authors cannot be updated'})
-    if(req.body.name != null){
-        if(req.body.name.firstName != null){
-            res.author.name.firstName = req.body.name.firstName
+router.patch('/:id', /*getAuthor,*/ async (request, response) =>{
+    //response.set('Allow', 'GET, HEAD').status(405).json({message: 'Authors cannot be updated'})
+    if(request.body.name != null){
+        if(request.body.name.firstName != null){
+            response.author.name.firstName = request.body.name.firstName
         }
-        if(req.body.name.surname != null){
-            res.author.name.surname = req.body.name.surname
+        if(request.body.name.surname != null){
+            response.author.name.surname = request.body.name.surname
         }
     }
-    if(req.body.publications != null){
-        res.author.publications = req.body.publications
+    if(request.body.publications != null){
+        response.author.publications = request.body.publications
     }
 
     try{
-        const updatedAuthor = await res.author.save()
-        res.json(updatedAuthor)
+        const updatedAuthor = await response.author.save()
+        response.json(updatedAuthor)
     }
     catch(error){
-        res.status(400).json({message: error.message})
+        response.status(400).json({message: error.message})
     }
 })
 
 // Remove a specific author (DELETE)
-router.delete('/:id', /*getAuthor,*/ async (req, res) =>{
-    //res.set('Allow', 'GET, HEAD').status(405).json({message: 'Authors cannot be deleted'})
+router.delete('/:id', /*getAuthor,*/ async (request, response) =>{
+    //response.set('Allow', 'GET, HEAD').status(405).json({message: 'Authors cannot be deleted'})
     try{
         
-        if(res.author == null){
-            res.status(200).json({message: "Object not existing"})
+        if(response.author == null){
+            response.status(200).json({message: "Object not existing"})
             return
         }
-        await res.author.remove()
-        res.json({message: 'Deleted author'})
+        await response.author.remove()
+        response.json({message: 'Deleted author'})
     }
     catch(error){
-        res.status(500).json({message: error.message})
+        response.status(500).json({message: error.message})
     }
 })
 
 // Reusable help method to retrieve a single author
-async function getAuthor (req, res, next){
+async function getAuthor (request, response, next){
     let authors, author
     try{
-        authors = await Author.find({"_id":req.params.id})
+        authors = await Author.find({"_id":request.params.id})
         author = authors[0]
         
         if(author == null){
-            return res.status(404).json({message: 'Cannot find author'})
+            return response.status(404).json({message: 'Cannot find author'})
         }
     }catch(error){
-        return res.status(500).json({message: error.message})
+        return response.status(500).json({message: error.message})
     }
-    res.author = author
+    response.author = author
+    next()
+}
+
+// Reusable help method to set options
+async function setOptions(request, response, next) {
+    response.status(200).options = 'GET,HEAD'
     next()
 }
 
--- server/routes/v1/books.js
+++ server/routes/v1/books.js
@@ -3,109 +3,125 @@ const router = express.Router()
 const Book = require('../../models/book')
 
 // Get list of all allowed methods (OPTIONS)
-router.options('/', async (req, res) => {
-    res.set('Allow', 'GET,HEAD,POST,PATCH,DELETE').status(200).json("GET,HEAD,POST,PATCH,DELETE")
+router.options('/', setOptions, async (request, response) => {
+    response.set('Allow', response.options).json(response.options)
+})
+router.options('/:id', setOptions, async (request, response) => {
+    response.set('Allow', response.options).json(response.options)
 })
 
 // Retrieve all books (GET)
-router.get('/', async (req, res) =>{
+router.get('/', async (request, response) =>{
     try{
         const books = await Book.find().populate('authors')
-        res.status(200).json(books)
+        response.status(200).json(books)
     }catch(error){
-        res.status(500).json({message: error.message})
+        response.status(500).json({message: error.message})
     }
 })
 
 // Retrieve a specific book (GET)
-router.get('/:id', getBook, async (req, res) =>{
-    res.json(res.book)
+router.get('/:id', getBook, async (request, response) =>{
+    response.json(response.book)
 })
 
 // Create a new book (POST)
-router.post('/', async (req, res) =>{
+router.post('/', async (request, response) =>{
     const book = new Book({
         _id: 999, // only temporary, is overwritten pre-save
-        isbn: req.body.isbn,
-        title: req.body.title,
-        category: req.body.category,
-        authors: req.body.authors,
-        edition: req.body.edition,
-        releaseYear: req.body.releaseYear,
-        status: req.body.status
+        isbn: request.body.isbn,
+        title: request.body.title,
+        category: request.body.category,
+        authors: request.body.authors,
+        edition: request.body.edition,
+        releaseYear: request.body.releaseYear,
+        status: request.body.status
     })
 
     try{
         const newBook = await book.save()
-        res.status(201).json(newBook)
+
+        let books = await Book.find({"_id":newBook._id}).populate('authors')  
+        let populatedBook = books[0]
+
+        response.status(201).json(populatedBook)
     }catch(error){
-        res.status(400).json({message: error.message})
+        response.status(400).json({message: error.message})
     }
 })
 
 // Update certain details of a specific book (PATCH)
-router.patch('/:id', getBook, async (req, res) =>{
-    if(req.body.isbn != null){
-        res.book.isbn = req.body.isbn
+router.patch('/:id', getBook, async (request, response) =>{
+    if(request.body.isbn != null){
+        response.book.isbn = request.body.isbn
     }
-    if(req.body.title != null){
-        res.book.title = req.body.title
+    if(request.body.title != null){
+        response.book.title = request.body.title
     }
-    if(req.body.category != null){
-        res.book.category = req.body.category
+    if(request.body.category != null){
+        response.book.category = request.body.category
     }
-    if(req.body.authors != null){
-        res.book.authors = req.body.authors
+    if(request.body.authors != null){
+        response.book.authors = request.body.authors
     }
-    if(req.body.edition != null){
-        res.book.edition = req.body.edition
+    if(request.body.edition != null){
+        response.book.edition = request.body.edition
     }
-    if(req.body.releaseYear != null){
-        res.book.releaseYear = req.body.releaseYear
+    if(request.body.releaseYear != null){
+        response.book.releaseYear = request.body.releaseYear
     }
-    if(req.body.status != null){
-        res.book.status = req.body.status
+    if(request.body.status != null){
+        response.book.status = request.body.status
     }
     
     try{
-        const updatedBook = await res.book.save()
-        res.json(updatedBook)
+        const updatedBook = await response.book.save()
+
+        let books = await Book.find({"_id":request.params.id}).populate('authors')  
+        let populatedBook = books[0]
+
+        response.json(populatedBook)
     }
     catch(error){
-        res.status(400).json({message: error.message})
+        response.status(400).json({message: error.message})
     }
 })
 
 // Remove a specific book (DELETE)
-router.delete('/:id', getBook, async (req, res) =>{
+router.delete('/:id', getBook, async (request, response) =>{
     try{
-        if(res.book == null){
-            res.status(200).json({message: "Object not existing"})
+        if(response.book == null){
+            response.status(200).json({message: "Object not existing"})
             return
         }
-        await res.book.remove()
-        res.json({message: 'Deleted book'})
+        await response.book.remove()
+        response.json({message: 'Deleted book'})
     }
     catch(error){
-        res.status(500).json({message: error.message})
+        response.status(500).json({message: error.message})
     }
 })
 
 // Reusable help method to retrieve a single book
-async function getBook (req, res, next){
+async function getBook (request, response, next){
     let books, book
     try{
-        console.log(req.params.id);
-        books = await Book.find({"_id":req.params.id}).populate('authors')  
+        books = await Book.find({"_id":request.params.id}).populate('authors')  
         book = books[0]
         
         if(book == null){
-            return res.status(404).json({message: 'Cannot find book'})
+            return response.status(404).json({message: 'Cannot find book'})
         }
     }catch(error){
-        return res.status(500).json({message: error.message})
+        return response.status(500).json({message: error.message})
     }
-    res.book = book
+    response.book = book
+    next()
+}
+
+// Reusable help method to set options
+async function setOptions(request, response, next) {
+    response.status(200).options = 'GET,HEAD,POST,PATCH,DELETE'
     next()
 }
 
