08716c36b7e708f3ea2ad286767b3c5e69b71975
nas <nadja.schindler@cp-bap.de>
2023-03-13 11:28:15 +0100

    Suchleiste eingefügt

--- client/data/books.js
+++ client/data/books.js
@@ -1,9 +1,34 @@
+const f = document.getElementById('form');
+const search = document.getElementById('searchId');
 const tilegrid = document.querySelector('.tilegrid');
 
-fetch('http://127.0.0.1:3000/books',{method:"GET"})
+function submitted(event) {
+  event.preventDefault();
+  console.log("button: " + search.value)
+  const result = []
+  data = fetch('http://127.0.0.1:3000/books',{method:"GET"})
   .then(response => response.json())
   .then(books => {
+    console.log(books)
+    if(search.value != null && search.value != ""){
     for (let book of books) {
+      if(book.title.includes(search.value)){
+        result.push(book)
+        console.log("result " + result)
+        deleteAllBooks()
+        searchedBooks()
+      }
+    }
+  }}
+  )
+  function deleteAllBooks(){
+    while (tilegrid.lastElementChild) {
+      tilegrid.removeChild(tilegrid.lastElementChild);
+    }
+  }
+  function searchedBooks() {
+    for (let book of result) {
+      console.log("Schleife 1")
       const tile = document.createElement('div');
       tile.classList.add('tile');
 
@@ -38,4 +63,49 @@ fetch('http://127.0.0.1:3000/books',{method:"GET"})
 
       tilegrid.appendChild(tile);
     }
-  });
+  }
+}
+
+  f.addEventListener('submit', submitted);
+  console.log("Searchvalue" + search.value)
+  data = fetch('http://127.0.0.1:3000/books',{method:"GET"})
+  .then(response => response.json())
+  .then(books => {
+    for (let book of books) {
+      console.log("Schleife 2")
+      const tile = document.createElement('div');
+      tile.classList.add('tile');
+
+      const title = document.createElement('h2');
+      title.textContent = book.title;
+      tile.appendChild(title);
+
+      const isbn = document.createElement('p');
+      isbn.textContent = 'ISBN: ' + book.isbn;
+      tile.appendChild(isbn);
+
+      const authors = document.createElement('p');
+      //authors.textContent = 'Authors: ' + book.authors.join(', ');
+      let authorNames = '';
+      for(let i = 0; i < book.authors.length; i++) {
+        let author = book.authors[i];
+        if(authorNames !== '') {
+          authorNames = authorNames + ', ';
+        }
+        authorNames = authorNames + author.name.firstName + ' ' + author.name.surname;
+      }
+      authors.textContent = 'Autoren: ' + authorNames;
+      tile.appendChild(authors);
+
+      const releaseYear = document.createElement('p');
+      releaseYear.textContent = 'Veröffentlicht: ' + book.releaseYear;
+      tile.appendChild(releaseYear);
+
+      tile.addEventListener('click', () => {
+        window.location.href = window.location.protocol + "//" + window.location.hostname + ":5500"+`/client/pages/bookDetails.html?id=${book._id}`
+      });
+
+      tilegrid.appendChild(tile);
+    }
+ })
+      
