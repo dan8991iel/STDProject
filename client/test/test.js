c5bf2bbe1fc39157fa43a83f0371a74a2d4c94e5
j-zupfer <108395649+j-zupfer@users.noreply.github.com>
2023-03-13 12:12:20 +0100

    Delete Test

--- client/test/test.js
+++
@@ -1,9 +0,0 @@
-//Mocha
-var assert = require('assert');
-describe('Array', function() {
-    describe('#indexOf()', function() {
-        isTypedArray('should return -1 when the value is not present', function () {
-            assert.equal([1, 2, 3].indexOf(4), -1);
-        });
-    });
-});
