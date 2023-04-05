3fc23e9b8d2b78273c44c72d92ccb5eba3cf520e
nas <nadja.schindler@cp-bap.de>
2023-04-05 15:04:51 +0200

    Test kompakter gemacht

--- client/tests/nas/systemtest/searchForBook.spec.ts
+++ client/tests/nas/systemtest/searchForBook.spec.ts
@@ -2,15 +2,18 @@ import { test, expect } from '@playwright/test';
 
 const LOCATOR_BOOK_TITLE = ".tile h2";
 const SEARCH_FIELD = "#search-input";
+const DELETE_SEARCH_FIELD_INPUT = "#clear-search-input"
 const URL = "http://127.0.0.1:5500/client/src/pages/books.html";
 
+const TESTRUNS = 2
+
 test.describe('search specific book', () => {
-    test('Open books.html, get the title from the 1. book, write this title in the searchfield, get the title of the book, which appears and check if the titles are equal', async ({
+    test('Open books.html, get the title from the first ' + TESTRUNS + ' books, write this title in the searchfield, get the title of the book, which appears and check if the titles are equal', async ({
         page,
     }) => {
-            await page.goto(URL);
-
-            const bookTitle = (await page.locator(LOCATOR_BOOK_TITLE).nth(0).innerText()).toString();
+        await page.goto(URL);
+        for(let i = 0; i < TESTRUNS; i++){
+            const bookTitle = (await page.locator(LOCATOR_BOOK_TITLE).nth(i).innerText()).toString();
 
             await expect(page.locator(SEARCH_FIELD)).toBeVisible({ timeout: 2000 });
 
@@ -19,23 +22,7 @@ test.describe('search specific book', () => {
 
             const searchedBookTitle = (await page.locator(LOCATOR_BOOK_TITLE).nth(0).innerText()).toString();
             expect(bookTitle).toEqual(searchedBookTitle);
-    })
-});
-
-test.describe('search specific book', () => {
-    test('Open books.html, get the title from the 2. book, write this title in the searchfield, get the title of the book, which appears and check if the titles are equal', async ({
-        page,
-    }) => {
-            await page.goto(URL);
-
-            const bookTitle = (await page.locator(LOCATOR_BOOK_TITLE).nth(1).innerText()).toString();
-
-            await expect(page.locator(SEARCH_FIELD)).toBeVisible({ timeout: 2000 });
-
-            await page.fill(SEARCH_FIELD, bookTitle);
-            await page.waitForTimeout(500);
-
-            const searchedBookTitle = (await page.locator(LOCATOR_BOOK_TITLE).nth(0).innerText()).toString();
-            expect(bookTitle).toEqual(searchedBookTitle);
+            await page.locator(DELETE_SEARCH_FIELD_INPUT).click();
+        }
     })
 });
